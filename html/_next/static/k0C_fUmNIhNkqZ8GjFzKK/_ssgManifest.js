edd650c7c22212c56696f2e01ef4f04df02dc9e2
Matt-Howell <73589713+Matt-Howell@users.noreply.github.com>
2024-02-10 12:07:58 -0800

    Add files via upload

---
+++ html/_next/static/k0C_fUmNIhNkqZ8GjFzKK/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002F404","\u002Fget\u002Ftemplates\u002F[id]","\u002Fget\u002Fwidgets\u002F[id]","\u002Ftags\u002F[tag]","\u002Ftemplates","\u002Ftemplates\u002F[id]","\u002Fwidgets","\u002Fwidgets\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
